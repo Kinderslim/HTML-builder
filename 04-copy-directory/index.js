fd26effc5c9a27b6ebc94945dcc59805225669a2
Kinderslim <Andreivsurvey@gmail.com>
2023-05-06 10:58:32 +1000

    feat: finished 4th task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,26 @@
+const fs = require('fs');
+const path = require('path');
+const fsPromises = require('node:fs/promises');
 
+let dirFrom = path.resolve(__dirname, 'files');
+console.log(dirFrom);
+let dirTo = path.resolve(__dirname, 'files-copy');
+
+function nothingFrom() {
+  fs.promises.readdir(dirTo).then(files => files.forEach(file => {
+    let filesNothing = path.resolve(dirTo, file);
+    fsPromises.unlink(filesNothing);
+  }));
+};
+
+function copyDir() {
+  fsPromises.mkdir(path.join(dirTo), { recursive: true });
+  nothingFrom();
+  fsPromises.readdir(dirFrom).then(files => files.forEach(file => {
+    let fileDirFrom = path.resolve(dirFrom, file);
+    let fileDirTo = path.resolve(dirTo, file);
+    fsPromises.copyFile(fileDirFrom, fileDirTo);
+  }));
+};
+
+copyDir();
